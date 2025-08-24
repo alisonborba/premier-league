d6782b4e6cd03a44faecad7e6c9165a91c72714c
Alison Borba <alisonborbaa@gmail.com>
2025-08-24 20:42:34 +0100

    original files

---
+++ frontend/src/index.tsx
@@ -0,0 +1,11 @@
+/** @format */
+
+import React from 'react';
+import { hydrateRoot } from 'react-dom/client';
+import { App } from './App';
+
+const container = document.getElementById('root');
+
+if (container) {
+  hydrateRoot(container, <App />);
+}
